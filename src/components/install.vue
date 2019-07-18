83945ef61f3a9aedc6672c73eb2e1fe893b6cbe4
EagleEye25 <jasonheybers@gmail.com>
2019-07-18 16:20:46 +0200

    added initial ping function

--- src/components/install.vue
+++ src/components/install.vue
@@ -4,18 +4,23 @@
     <h1 style="color:white"> Settings </h1>
     <br>
     <div>
-      <md-card>
-        <md-field class="center" style="max-width:40%">
-          <label for="serverAddy">Server Address</label>
-          <md-input class="serverAddy" v-model="serverAddy"/>
-        </md-field>
-        <md-button class="md-raised md-primary" :disabled="!serverAddy" @click="setDone('second', 'third')">Continue</md-button>
+      <md-card class="md-layout-item md-size-50 md-small-size-100 center box">
+          <div style=padding-left:25%>
+            <md-field style="width: 70%">
+              <label>Server Address</label>
+              <md-input v-model="serverAddy"/>
+            </md-field>
+          </div>
+
+        <md-button class="md-raised md-primary"  @click="testCon">Continue</md-button>
       </md-card>
     </div>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
+import { fail } from 'assert';
 
   export default {
     name: 'install',
@@ -30,7 +35,16 @@
     },
 
     methods: {
-      
+      async testCon() {
+        return await this.$awn.asyncBlock(axios.get(`${this.serverAddy}/app/test/`)
+          .then((result) => {
+            console.log('success');
+            return true
+          }).catch((err) => {
+            console.log('fail')
+            return false
+          }), null, null);
+      }
     }
   }
 </script>
@@ -43,6 +57,12 @@
     margin-left: auto;
     margin-right: auto;
     /* width: 50%; */
+  }
+
+
+  .box {
+    -webkit-border-radius: 6px;;
+    border-radius: 6px;;
   }
 
 </style>
